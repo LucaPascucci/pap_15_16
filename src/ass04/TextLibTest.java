9d37bea2b52a12fe63dc5f45cd3c2d1843fb679f
LucaPascucci <luca.pascucci@studio.unibo.it>
2016-04-20 15:29:48 +0200

    Sistemati rimbalzi Commentato ASS04

--- src/ass04/TextLibTest.java
+++ src/ass04/TextLibTest.java
@@ -6,25 +6,25 @@ public class TextLibTest {
 
     private static final int MAX_X = 75;
     private static final int MAX_Y = 25;
-    private static final String[] WORDS = {"Mare","Sole","Luna","Nuvole","vento"};
+    private static final String[] WORDS = {"Mare","Sole","Luna","Nuvole","Vento"};
 
 	public static void main(String[] args) throws Exception {
-
-        //TODO commentare
 
 		TextLib lib = TextLibFactory.getInstance();
         lib.cls();
 
-        IntStream.rangeClosed(0,MAX_X).forEach(i -> {
+        //creazione dei bordi della console
+        IntStream.rangeClosed(1,MAX_X).forEach(i -> {
             lib.writeAt(i,0,"-");
             lib.writeAt(i,MAX_Y,"-");
         });
 
-        IntStream.rangeClosed(0,MAX_Y).forEach(i -> {
+        IntStream.rangeClosed(1,MAX_Y).forEach(i -> {
             lib.writeAt(1,i,"|");
             lib.writeAt(MAX_X,i,"|");
         });
 
+        //creazione di un thread per ogni parola immessa o presente
         if (args.length != 0) {
             IntStream.range(0, args.length)
                     .forEach(i -> new WordAgent(args[i], lib, MAX_X, MAX_Y).start());
--- src/ass04/WordAgent.java
+++ src/ass04/WordAgent.java
@@ -7,7 +7,6 @@ import java.util.Random;
  * Created by Luca on 19/04/16.
  */
 
-//TODO commentare
 public class WordAgent extends Thread {
 
     private static final Random RANDOM = new Random();
@@ -15,7 +14,7 @@ public class WordAgent extends Thread {
     private final TextLib world;
     private final Color color;
     private final String cleaner;
-    private final long wait_sleep;
+    private final long wait_sleep; //tempo in millisec di attesa del thread
     private final int max_X;
     private final int max_Y;
     private int pos_X;
@@ -24,14 +23,15 @@ public class WordAgent extends Thread {
     private int step_Y;
 
     public WordAgent (String text, TextLib world, int max_x, int max_y){
-        this.wait_sleep = this.randomBetweenTwoNumbers(50,200); //valore random tra 50 e 200
+        this.wait_sleep = this.randomBetweenTwoNumbers(50,200);
         this.word = text;
         this.world = world;
-        this.color = Color.values()[RANDOM.nextInt(Color.values().length)];
+        this.color = Color.values()[RANDOM.nextInt(Color.values().length)]; //genera un valore random incluso tra 0 e length-1
         this.max_X = max_x;
         this.max_Y = max_y;
+        //genero la posizione della parola
         this.pos_X = this.randomBetweenTwoNumbers(2,this.max_X - this.word.length());
-        this.pos_Y = this.randomBetweenTwoNumbers(2,this.max_Y - 1);
+        this.pos_Y = this.randomBetweenTwoNumbers(2,this.max_Y - 2);
         this.step_X = Math.random() < 0.5? 1 : -1;
         this.step_Y = Math.random() < 0.5? 1 : -1;
         this.cleaner = this.word.replaceAll(".", " ");
@@ -42,8 +42,12 @@ public class WordAgent extends Thread {
         super.run();
         try {
             while (true){
+                //scrivo la parola su console
                 this.world.writeAt(this.pos_X,this.pos_Y,this.word,this.color);
                 sleep(this.wait_sleep);
+                //dopo un tot di millisecondi copre la parola con una stringa vuota (Evita l'effetto scia)
+                this.world.writeAt(this.pos_X,this.pos_Y, this.cleaner);
+                //sposto la posizione per il ciclo successivo
                 updatePos();
             }
         } catch (Exception ex){
@@ -51,30 +55,31 @@ public class WordAgent extends Thread {
         }
     }
 
+    //aggiorna la posizione aumentadola dello step
     private synchronized void updatePos(){
-        this.world.writeAt(this.pos_X,this.pos_Y, this.cleaner);
         this.pos_X += this.step_X;
         this.pos_Y += this.step_Y;
         this.applyConstraints();
     }
 
-    //TODO sistemare impatti con i bordi Y
+    //controllo per il rimbalzo su ogni lato della console
     private void applyConstraints(){
-        if (this.pos_X + this.word.length() > this.max_X){
-            this.pos_X = this.max_X - this.word.length();
+        if ((this.pos_X + this.word.length()) > this.max_X - 3){
+            this.pos_X = (this.max_X - 3) - this.word.length();
             this.step_X = -this.step_X;
-        } else if (this.pos_X < 2){
-            this.pos_X = 2;
+        } else if (this.pos_X < 3){
+            this.pos_X = 3;
             this.step_X = -this.step_X;
-        } else if (this.pos_Y > this.max_Y - 2){
-            this.pos_Y = this.max_Y - 2;
+        } else if (this.pos_Y > this.max_Y - 3){
+            this.pos_Y = this.max_Y - 3;
             this.step_Y = -this.step_Y;
-        } else if (this.pos_Y < 2){
-            this.pos_Y = 2;
+        } else if (this.pos_Y < 3){
+            this.pos_Y = 3;
             this.step_Y = -this.step_Y;
         }
     }
 
+    //genera un valore random compreso tra i due valori
     private int randomBetweenTwoNumbers(int min, int max){
         return RANDOM.nextInt(max - min) + min;
     }
